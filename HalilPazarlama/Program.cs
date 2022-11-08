becbea82bf1f961119579623c6989d1436ef064a
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2022-11-08 22:48:59 +0300

    .

--- HalilPazarlama/Program.cs
+++ HalilPazarlama/Program.cs
@@ -20,8 +20,30 @@ namespace HalilPazarlama
             Supurge s = new Supurge();
             Televizyon tv = new Televizyon();
             #endregion
+            #region ÖRNEK LİST
+            //BulasikMakinesi bulasik = new BulasikMakinesi();
+            //Personel p1 = new Personel();
+            //p1.isim = "Hasan Hüseyin";
+            //p1.soyad = "BADAK";
+            //p1.telefonNumarasi = "+90 537 259 6733";
+            //p1.yas = 28;
 
-            
+            //Personel p2 = new Personel();
+            //p2.isim = "Melike";
+            //p2.soyad = "BADAK";
+            //p2.telefonNumarasi = "+90 507 097 8476";
+            //p2.yas = 26;
+
+            //List<Personel> personeller = new List<Personel>();
+            //personeller.Add(p1);
+            //personeller.Add(p2);
+            //Console.WriteLine($"Eleman Sayısı = {personeller.Count}");
+            //for (int i = 0; i < personeller.Count; i++)
+            //{
+            //    Console.WriteLine($"{personeller[i].isim} {personeller[i].soyad} {personeller[i].yas} {personeller[i].telefonNumarasi}");
+            //}
+            #endregion
+
 
 
 
